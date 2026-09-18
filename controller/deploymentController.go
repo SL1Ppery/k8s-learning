4618e40c28aa65f0c70057b8d64373db48bb9e71
SL1Ppery <lipengsun657@gmail.com>
2026-09-18 14:12:55 +0800

    Add fake controller update

--- controller/deploymentController.go
+++ controller/deploymentController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"k8s-learning/client"
 	"k8s-learning/k8sinterface"
 	"k8s-learning/resource"
@@ -15,11 +16,21 @@ func (dc *DeploymentController) Handle(r k8sinterface.Resource) {
 	dc.Reconcile(dep)
 }
 
-func (dc *DeploymentController) Reconcile(dep *resource.Deployment) {
-	currentDep, err := dc.Client.GetDeployment(dep.GetName())
+func (dc *DeploymentController) Reconcile(desireddep *resource.Deployment) {
+	currentDep, err := dc.Client.GetDeployment(desireddep.GetName())
 	if err != nil {
-		dc.Client.CreateDeployment(dep)
+		dc.Client.CreateDeployment(desireddep)
 		return
 	}
 	currentDep.GetInfo()
+
+	if desireddep.Replicas != currentDep.Replicas {
+		currentDep.Replicas = desireddep.Replicas
+		err = dc.Client.UpdateDeployment(currentDep)
+		if err != nil {
+			fmt.Printf("更新deployment状态失败: %v\n", err)
+			return
+		}
+		fmt.Printf("deployment状态 %s 已更新\n", desireddep.GetName())
+	}
 }
--- controller/podController.go
+++ controller/podController.go
@@ -16,12 +16,24 @@ func (pc *PodController) Handle(r k8sinterface.Resource) {
 	pc.Reconcile(pod)
 }
 
-func (pc *PodController) Reconcile(pod *resource.Pod) {
-	currentPod, err := pc.Client.GetPod(pod.Metadata.Name)
+func (pc *PodController) Reconcile(desiredpod *resource.Pod) {
+	currentPod, err := pc.Client.GetPod(desiredpod.Metadata.Name)
 	if err != nil {
-		pc.Client.CreatePod(pod)
-		fmt.Printf("pod %s 已创建\n", pod.Metadata.Name)
+		pc.Client.CreatePod(desiredpod)
+		fmt.Printf("pod %s 已创建\n", desiredpod.Metadata.Name)
 		return
 	}
 	currentPod.GetInfo()
+	if desiredpod.Status.Phase != currentPod.Status.Phase {
+		fmt.Printf("Pod: %s 当前状态与期望值不符,当前：%s 期望：%s", desiredpod.Metadata.Name, currentPod.Status.Phase, desiredpod.Status.Phase)
+	}
+	currentPod.Status.Phase = desiredpod.Status.Phase
+	err = pc.Client.UpdatePod(currentPod)
+	if err != nil {
+		fmt.Printf("更新pod状态失败: %v\n", err)
+		return
+	} else {
+		fmt.Printf("pod:%s 状态已更新\n", desiredpod.Metadata.Name)
+	}
+
 }

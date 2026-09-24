45830bd56d5a5d210b114a9615c02a1adfa52104
SL1Ppery <lipengsun657@gmail.com>
2026-09-24 10:50:58 +0800

    test api -> event -> queue -> worker -> controller

--- main.go
+++ main.go
@@ -2,61 +2,42 @@ package main
 
 import (
 	"fmt"
-	"k8s-learning/client"
 	"k8s-learning/controller"
 	"k8s-learning/factory"
 	"k8s-learning/fakeapi"
 	myparser "k8s-learning/parser"
 	"k8s-learning/resource"
+	"k8s-learning/worker"
 )
 
 func main() {
+	//init
 	registry := factory.NewRegistry()
 	registry.Register("Pod", resource.NewPod)
 	registry.Register("Deployment", resource.NewDeployment)
 	registry.Register("ReplicaSet", resource.NewReplicaSet)
+	apiserver := fakeapi.NewFakeAPIServer()
 	controllerManager := controller.NewControllerManager()
-	fakeClient := &client.FakeClient{}
-	podController := &controller.PodController{Client: fakeClient}
-	deploymentController := &controller.DeploymentController{Client: fakeClient}
-	replicaSetController := &controller.ReplicaSetController{Client: fakeClient}
+	podController := &controller.PodController{FakeAPIServer: apiserver}
+	deploymentController := &controller.DeploymentController{FakeAPIServer: apiserver}
+	replicaSetController := &controller.ReplicaSetController{FakeAPIServer: apiserver}
 	controllerManager.Registry("Pod", podController)
 	controllerManager.Registry("Deployment", deploymentController)
 	controllerManager.Registry("ReplicaSet", replicaSetController)
+	worker := worker.Worker{
+		Queue:             *apiserver.Event_handel.Queue,
+		ControllerManager: *controllerManager,
+		FakeAPIServer:     *apiserver,
+	}
+	go worker.Run()
+
 	resources := myparser.Parser("test.yaml", registry)
-	server := fakeapi.NewFakeAPIServer()
 	for _, r := range resources {
-		server.Create(r)
+		apiserver.Create(r)
 	}
-	pods := server.Cluster.Pods
-	for _, pod := range pods {
-		pod.GetInfo()
-	}
-	r, _ := server.Get("Deployment", "ruoyi", "ruoyi")
-	r.GetInfo()
-	dep := resource.Deployment{
-		APIVersion: "apps/v1",
-		Kind:       "Deployment",
-		Metadata:   resource.Metadata{Name: "ruoyi", Namespace: "prod"},
-		Replicas:   3,
-	}
-	server.Update(&dep)
-	r, _ = server.Get("Deployment", "prod", "ruoyi")
-	r.GetInfo()
-	server.Delete("Deployment", "prod", "ruoyi")
-	r, err := server.Get("Deployment", "prod", "ruoyi")
+	pod, err := apiserver.Get("Pod", "default", "nginx")
 	if err != nil {
-		fmt.Println("delete falsed:", err)
+		fmt.Println(err)
 	}
-	// wg := sync.WaitGroup{}
-	// wg.Add(1)
-	// wq := workqueue.NewWorkQueue(10)
-	// worker := &worker.Worker{Queue: *wq}
-	// go func() {
-	// 	defer wg.Done()
-	// 	worker.Run()
-	// }()
-	// wq.Add("default/pod1")
-	// wq.Add("default/pod2")
-	// wg.Wait()
+	pod.GetInfo()
 }

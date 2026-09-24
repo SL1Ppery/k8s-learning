9a70675efb4cdf33e6298eff8cc12f3f299b17aa
SL1Ppery <lipengsun657@gmail.com>
2026-09-24 10:43:53 +0800

    Use declared Cluster

--- fakeCluster/fakeCluster.go
+++ fakeCluster/fakeCluster.go
@@ -12,38 +12,23 @@ type FakeCluster struct {
 	ReplicaSets []resource.ReplicaSet
 }
 
-var Cluster = FakeCluster{
-	Pods: []resource.Pod{
-		{APIVersion: "v1", Kind: "Pod", Metadata: resource.Metadata{Name: "nginx-replicaset-feu24", Namespace: "default", Labels: map[string]string{"app": "nginx"}}, Status: resource.Status{Phase: "Running"}},
-		{APIVersion: "v1", Kind: "Pod", Metadata: resource.Metadata{Name: "mysql", Namespace: "default", Labels: map[string]string{"app": "mysql"}}, Status: resource.Status{Phase: "Pending"}},
-	},
-	Deployments: []resource.Deployment{
-		{APIVersion: "apps/v1", Kind: "Deployment", Metadata: resource.Metadata{Name: "nginx-deployment", Namespace: "default"}, Replicas: 3},
-		{APIVersion: "apps/v1", Kind: "Deployment", Metadata: resource.Metadata{Name: "ruoyi", Namespace: "ruoyi"}, Replicas: 3},
-	},
-	ReplicaSets: []resource.ReplicaSet{
-		{APIVersion: "apps/v1", Kind: "ReplicaSet", Metadata: resource.Metadata{Name: "nginx-replicaset", Namespace: "default"}, Replicas: 2, Selector: map[string]string{"app": "nginx"}},
-		{APIVersion: "apps/v1", Kind: "ReplicaSet", Metadata: resource.Metadata{Name: "ruoyi-replicaset", Namespace: "ruoyi"}, Replicas: 2, Selector: map[string]string{"app": "ruoyi"}},
-	},
-}
-
 func NewFakeCluster() *FakeCluster {
 	return &FakeCluster{}
 }
 
-func GetFakeClusterPods() []resource.Pod {
-	return Cluster.Pods
+func (f *FakeCluster) GetFakeClusterPods() []resource.Pod {
+	return f.Pods
 }
 
-func GetFakeClusterDeployment() []resource.Deployment {
-	return Cluster.Deployments
+func (f *FakeCluster) GetFakeClusterDeployment() []resource.Deployment {
+	return f.Deployments
 }
 
-func GetFakeClusterReplicaSets() []resource.ReplicaSet {
-	return Cluster.ReplicaSets
+func (f *FakeCluster) GetFakeClusterReplicaSets() []resource.ReplicaSet {
+	return f.ReplicaSets
 }
 
-func GetResourceByMetadata(key string) k8sinterface.Resource {
+func (f *FakeCluster) GetResourceByMetadata(key string) k8sinterface.Resource {
 	parts := strings.SplitN(key, "/", 2)
 
 	if len(parts) != 2 {
@@ -53,26 +38,43 @@ func GetResourceByMetadata(key string) k8sinterface.Resource {
 	namespace := parts[0]
 	name := parts[1]
 
-	for _, pod := range Cluster.Pods {
-		if pod.Metadata.Namespace == namespace &&
-			pod.Metadata.Name == name {
-			return &pod
+	for i := range f.Pods {
+		if f.Pods[i].Metadata.Namespace == namespace &&
+			f.Pods[i].Metadata.Name == name {
+			return &f.Pods[i]
 		}
 	}
 
-	for _, dep := range Cluster.Deployments {
-		if dep.Metadata.Namespace == namespace &&
-			dep.Metadata.Name == name {
-			return &dep
+	for i := range f.Deployments {
+		if f.Deployments[i].Metadata.Namespace == namespace &&
+			f.Deployments[i].Metadata.Name == name {
+			return &f.Deployments[i]
 		}
 	}
 
-	for _, rs := range Cluster.ReplicaSets {
-		if rs.Metadata.Namespace == namespace &&
-			rs.Metadata.Name == name {
-			return &rs
+	for i := range f.ReplicaSets {
+		if f.ReplicaSets[i].Metadata.Namespace == namespace &&
+			f.ReplicaSets[i].Metadata.Name == name {
+			return &f.ReplicaSets[i]
 		}
 	}
 
 	return nil
+}
+func (fc *FakeCluster) GetPodsByLabels(labels map[string]string) (*[]resource.Pod, error) {
+	var matchedPods []resource.Pod
+	fakeClusterPods := fc.GetFakeClusterPods()
+	for i := range fakeClusterPods {
+		match := true
+		for k, v := range labels {
+			if fakeClusterPods[i].Metadata.Labels[k] != v {
+				match = false
+				break
+			}
+		}
+		if match {
+			matchedPods = append(matchedPods, fakeClusterPods[i])
+		}
+	}
+	return &matchedPods, nil
 }
